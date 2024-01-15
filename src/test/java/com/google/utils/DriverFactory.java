65b0de2561b3062d89af9ef4ff55ab0f8f0a0d68
Oleh Velmyk <oleh.velmyk@gmail.com>
2024-01-15 12:33:09 +0200

    Added edgeOptions for EdgeDriver

--- src/test/java/com/google/utils/DriverFactory.java
+++ src/test/java/com/google/utils/DriverFactory.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
@@ -29,7 +30,11 @@ public class DriverFactory {
                 return new ChromeDriver();
             case "edge":
                 WebDriverManager.edgedriver().setup();
-                return new EdgeDriver();
+                EdgeOptions edgeOptions = new EdgeOptions()
+                        .addArguments("--no-sandbox")
+                        .addArguments("--disable-dev-shm-usage");
+//                        .addArguments("--remote-debugging-port=<port>");
+                return new EdgeDriver(edgeOptions);
             case "ie":
             case "internet explorer":
                 WebDriverManager.iedriver().setup();
